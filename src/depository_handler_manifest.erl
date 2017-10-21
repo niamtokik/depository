beb44fbd537fc5a96e3bae6256d835cb1a8eaaf0
niamtokik <mk@steepath.eu>
2017-10-21 16:07:57 +0200

    insert manifest v2 with some example and type definition.

--- src/depository_handler_manifest.erl
+++ src/depository_handler_manifest.erl
@@ -22,6 +22,14 @@ init(#{ method := <<"HEAD">>
 	io:format("reference: ~p~n", [Reference]),
 	depository_handler_default:init(Req, Opts);
 
+% put a manifest 
+init(#{ method := <<"PUT">> } = Req, Opts) ->
+	depository_handler_default:init(Req, Opts);
+
+% delete a manifest
+init(#{ method := <<"DELETE">> } = Req, Opts) ->
+	depository_handler_default:init(Req, Opts);
+
 % else we return a 404 not found
 init(Req, Opts) ->
 	{ok, Req, Opts}.
--- src/manifest_v2.erl
+++ src/manifest_v2.erl
@@ -5,4 +5,59 @@
 -module(manifest_v2).
 -compile(export_all).
 
+-type schema_version() :: integer().
+-type media_type() :: iolist().
+-type config() :: map().
+-type digest() :: iolist().
+-type layer() :: map().
+-type layers() :: [ layer(), ... ].
+-type urls() :: list().
 
+media_types() ->
+	[<<"application/vnd.docker.distribution.manifest.v1+json">>
+	,<<"application/vnd.docker.distribution.manifest.v2+json">>
+	,<<"application/vnd.docker.distribution.manifest.list.v2+json">>
+	,<<"application/vnd.docker.container.image.v1+json">>
+	,<<"application/vnd.docker.image.rootfs.diff.tar.gzip">>
+	,<<"application/vnd.docker.image.rootfs.foreign.diff.tar.gzip">>
+	,<<"application/vnd.docker.plugin.v1+json">>].
+
+digest() ->
+	<<"sha256:e692418e4cbaf90ca69d05a66403747baa33ee08806650b51fab815ad7fc331f">>.
+
+platform() ->
+	#{ architecture => <<"ppc64le">>
+	 , os => <<"linux">>
+	}.
+
+manifest() ->
+	#{ mediaType => <<"application/vnd.docker.distribution.manifest.v2+json">>
+	 , size => 7143
+	 , digest => digest()
+	 , platform => platform()
+	}.
+
+manifest_list() ->
+	#{ schemaVersion => 2
+	 , mediaType => <<"">>
+	 , manifests => [ manifest(), manifest() ]
+	}.
+
+config() ->
+	#{ mediaType => <<"application/vnd.docker.container.image.v1+json">>
+	 , size => 7023
+	 , digest => digest()
+	}.
+
+layer() ->
+	#{ mediaType => <<"application/vnd.docker.image.rootfs.diff.tar.gzip">>
+	 , size => 32654
+	 , digest => digest()
+	}.
+
+image_manifest() ->
+	#{ schemaVersion => 2
+	 , mediaType => <<"application/vnd.docker.distribution.manifest.v2+json">>
+	 , config => config()
+	 , layers => [ layer(), layer() ]
+	}.
